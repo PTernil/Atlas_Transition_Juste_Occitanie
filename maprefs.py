4d3543f3943d7f9cb9798538f7ec8379471522f9
PTernil <paul.ternil@ademe.fr>
2026-04-01 08:52:44 +0200

    Create maprefs.py

---
+++ maprefs.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 31 08:46:05 2026
+
+@author: ternilp
+"""
+
+grids={
+'reg' : {'edgecolor':'#232323', 'linewidth':2.6},
+'dep' : {'edgecolor':'#232323', 'linewidth':1.3},
+'com' : {'edgecolor':'#232323', 'linewidth':0.1625},
+'iris' : {'edgecolor':'#232323', 'linewidth':0}, 
+'pays_v' : {'edgecolor':'#232323', 'linewidth':1.3},
+'reg_v' : {'edgecolor':'#232323', 'linewidth':1.95},
+'dep_v' : {'edgecolor':'#232323', 'linewidth':0.1625},
+'pref' : {'color':'#444444', 'marker':'s', 'markersize':200, 'fontsize':40},
+'s_pref' : {'color':'#666666', 'marker':'o', 'markersize':100, 'fontsize':30}
+}
